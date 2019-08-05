13d1528e08b741b3798db2f9ca167f7e4cf5ab4d
聂红强 <36918549+NRatel@users.noreply.github.com>
2019-08-05 21:13:39 +0800

    Update test.lua

--- game/test/test.lua
+++ game/test/test.lua
@@ -1,6 +1,6 @@
 local http = require "resty.http"
 local httpc = http.new()
-local res, err = httpc:request_uri("http://www.nratel.com:81/game", {
+local res, err = httpc:request_uri("http://127.0.0.1:80/game", {
     method = "POST",
     args = { a = 111, b = 'bbb' },
     body = { c = 222, d = 'ddd' },
@@ -11,10 +11,12 @@ local res, err = httpc:request_uri("http://www.nratel.com:81/game", {
     keepalive_pool = 10
 })
 
-ngx.print("res.status: ", res.status)
+if (res) then
+    ngx.print("res.status: ", res.status)
 
-if 200 ~= res.status then
-    ngx.exit(res.status)
+    if 200 ~= res.status then
+        ngx.exit(res.status)
+    end
+
+    ngx.print("res.body: ", res.body)
 end
-
-ngx.print("res.body: ", res.body)
