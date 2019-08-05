6f0db4259174c7be24cb8568beea63ef9b0baecc
聂红强 <36918549+NRatel@users.noreply.github.com>
2019-08-05 20:11:14 +0800

    调整结构

--- game/main.lua
+++
@@ -1,2 +0,0 @@
-ngx.say("<p>hello nengneng!</p>")
-ngx.say("<p>你是个猪八戒!</p>")
---
+++ web/test/test.lua
@@ -0,0 +1,19 @@
+ngx.req.read_body()
+local args, err = ngx.req.get_uri_args()
+
+local http = require "resty.http"
+local httpc = http.new()
+local res, err = httpc:request_uri("http://127.0.0.1:80/game", {
+    method = "POST",
+    body = args.data,
+})
+
+if 200 ~= res.status then
+    ngx.exit(res.status)
+end
+
+if args.key == res.body then
+    ngx.say("valid request")
+else
+    ngx.say("invalid request")
+end
