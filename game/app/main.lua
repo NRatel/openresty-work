d9309a4deeb22b8d85b25159a09813b3aae22cad
聂红强 <36918549+NRatel@users.noreply.github.com>
2019-08-05 21:08:25 +0800

    测试

--- game/app/main.lua
+++ game/app/main.lua
@@ -1,1 +1,18 @@
 
+--GET arg
+local getArgs = ngx.req.get_uri_args()
+for k,v in pairs(getArgs) do
+    ngx.say("[GET ] key:", k, " v:", v)
+end
+
+--POST arg
+ngx.req.read_body() -- 解析 body 参数之前一定要先读取 body
+local postArgs = ngx.req.get_post_args()
+for k,v in pairs(getArgs) do
+    ngx.say("[POST ] key:", k, " v:", v)
+end
+
+--POST body
+
+
+ 
--- game/test/test.lua
+++ game/test/test.lua
@@ -1,19 +1,20 @@
-ngx.req.read_body()
-local args, err = ngx.req.get_uri_args()
-
 local http = require "resty.http"
 local httpc = http.new()
-local res, err = httpc:request_uri("http://127.0.0.1:81/game", {
+local res, err = httpc:request_uri("http://www.nratel.com:81/game", {
     method = "POST",
-    body = args.data,
+    args = { a = 111, b = 'bbb' },
+    body = { c = 222, d = 'ddd' },
+    -- headers = {
+    --     ["Content-Type"] = "application/x-www-form-urlencoded",
+    -- },
+    keepalive_timeout = 60,
+    keepalive_pool = 10
 })
+
+ngx.print("res.status: ", res.status)
 
 if 200 ~= res.status then
     ngx.exit(res.status)
 end
 
-if args.key == res.body then
-    ngx.say("valid request")
-else
-    ngx.say("invalid request")
-end
+ngx.print("res.body: ", res.body)
