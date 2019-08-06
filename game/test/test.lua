5cdc92cbfb49298fb6e3123292acecf205114fc6
聂红强 <36918549+NRatel@users.noreply.github.com>
2019-08-06 15:41:27 +0800

    测试

--- game/test/test.lua
+++ game/test/test.lua
@@ -1,6 +1,6 @@
 local http = require "resty.http"
 local httpc = http.new()
-local res, err = httpc:request_uri("http://127.0.0.1:80/game", {
+local res, err = httpc:request_uri("http://127.0.0.1:81/game", {
     method = "POST",
     args = ngx.encode_args({ a = 111, b = 'bbb' }),
     body = ngx.encode_args({ c = 222, d = 'ddd' }),
