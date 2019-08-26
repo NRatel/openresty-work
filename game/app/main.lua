e60da041cb765658c7c651675a90576650001d83
聂红强 <36918549+NRatel@users.noreply.github.com>
2019-08-26 17:08:42 +0800

    test

--- game/app/main.lua
+++ game/app/main.lua
@@ -21,34 +21,18 @@ end
 local pb = require "pb"
 local protoc = require "protoc"
 
--- load schema from text
-assert(protoc:load [[
-   message Phone {
-      optional string name        = 1;
-      optional int64  phonenumber = 2;
-   }
-   message Person {
-      optional string name     = 1;
-      optional int32  age      = 2;
-      optional string address  = 3;
-      repeated Phone  contacts = 4;
-   } ]])
 
--- lua table data
-local data = {
-   name = "ilse",
-   age  = 18,
-   contacts = {
-      { name = "alice", phonenumber = 12312341234 },
-      { name = "bob",   phonenumber = 45645674567 }
-   }
-}
+local file = io.open("./protos/test.proto", "r")
+local str = file:read("*a")
+file:close()
+
+ngx.say(str)
 
 -- encode lua table data into binary format in lua string and return
-local bytes = assert(pb.encode("Person", data))
-ngx.say(pb.tohex(bytes))
+-- local bytes = assert(pb.encode("Person", data))
+-- ngx.say(pb.tohex(bytes))
 
--- and decode the binary data back into lua table
-local data2 = assert(pb.decode("Person", bytes))
-ngx.say(serpent.block(data2))
+-- -- and decode the binary data back into lua table
+-- local data2 = assert(pb.decode("Person", bytes))
+-- ngx.say(serpent.block(data2))
  
