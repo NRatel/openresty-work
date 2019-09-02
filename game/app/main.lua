df04bbad189af13e5fb96fce54e5c1d7fdea982d
聂红强 <36918549+NRatel@users.noreply.github.com>
2019-09-02 16:31:57 +0800

    Update main.lua

--- game/app/main.lua
+++ game/app/main.lua
@@ -12,11 +12,10 @@ ngx.req.read_body() -- 解析 body 参数之前一定要先读取 body
 local postArgs = ngx.req.get_post_args()
 for k, v in pairs(getArgs) do
     ngx.say("[POST ] key:", k, " v:", v)
-end  
+end
 
 --POST body
 -----------------------------------------------------------------------
-
 --注册pb
 local prefix = ngx.config.prefix()  --"/home/nratel/openresty-work/game/"
 require("app.protobuf.register").register(prefix .. "app/protobuf/proto.pb");
@@ -32,3 +31,44 @@ local bytes = assert(pb.encode("msg.Login_C", data))
 ngx.say(pb.tohex(bytes))
 local data2 = assert(pb.decode("msg.Login_C", bytes))
 ngx.say(serpent.block(data2))
+
+-----------------------------------------------------------------------
+local redis = require "resty.redis"
+local red = redis:new()
+
+red:set_timeout(1000) -- 1 sec
+
+local ok, err = red:connect("127.0.0.1", 6379)
+if not ok then
+    ngx.say("failed to connect: ", err)
+    return
+end
+
+-- 请注意这里 auth 的调用过程
+local count
+count, err = red:get_reused_times()
+if 0 == count then
+    ok, err = red:auth("password")
+    if not ok then
+        ngx.say("failed to auth: ", err)
+        return
+    end
+elseif err then
+    ngx.say("failed to get reused times: ", err)
+    return
+end
+
+ok, err = red:set("dog", "an animal")
+if not ok then
+    ngx.say("failed to set dog: ", err)
+    return
+end
+
+ngx.say("set result: ", ok)
+
+-- 连接池大小是100个，并且设置最大的空闲时间是 10 秒
+local ok, err = red:set_keepalive(10000, 100)
+if not ok then
+    ngx.say("failed to set keepalive: ", err)
+    return
+end
