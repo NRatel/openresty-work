76beb59b5e66d02690ef01874aacc73f13ad14f2
聂红强 <36918549+NRatel@users.noreply.github.com>
2019-09-17 10:24:00 +0800

    Update content.lua

--- game/app/phase/content.lua
+++ game/app/phase/content.lua
@@ -12,11 +12,44 @@
 
 --------------------------------------------------------------------------
 
-require("app.test.request_arg")
+-- require("app.test.request_arg")
 
-require("app.test.pb")
+-- require("app.test.pb")
 
-require("app.test.mysql")
+-- require("app.test.mysql")
 
 -- require("app.test.redis")
 
+local mysql = require "resty.mysql"
+local db, err = mysql:new()
+if not db then
+    ngx.say("failed to instantiate mysql: ", err)
+    return
+end
+
+db:set_timeout(1000) -- 1 sec
+
+-- or connect to a unix domain socket file listened
+-- by a mysql server:
+--     local ok, err, errcode, sqlstate =
+--           db:connect{
+--              path = "/path/to/mysql.sock",
+--              database = "ngx_test",
+--              user = "ngx_test",
+--              password = "ngx_test" }
+local ok, err, errcode, sqlstate = db:connect {
+    host = "127.0.0.1",
+    port = 3306,
+    database = "test",
+    user = "test",
+    password = "test",
+    charset = "utf8",
+    max_packet_size = 1024 * 1024,
+}
+
+if not ok then
+    ngx.say("failed to connect: ", err, ": ", errcode, " ", sqlstate)
+    return
+end
+
+ngx.say("connected to mysql.")
