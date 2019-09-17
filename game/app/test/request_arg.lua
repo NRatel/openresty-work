8121a3661b5025efece95e033bbd1797eb0addda
聂红强 <36918549+NRatel@users.noreply.github.com>
2019-09-17 13:50:35 +0800

    test

--- game/app/test/request_arg.lua
+++ game/app/test/request_arg.lua
@@ -1,16 +1,20 @@
---GET arg
-local getArgs = ngx.req.get_uri_args()
-for k, v in pairs(getArgs) do
-    ngx.say("[GET ] key:", k, " v:", v)
+local _M = {}
+
+function _M:test()
+    --GET arg
+    local getArgs = ngx.req.get_uri_args()
+    for k, v in pairs(getArgs) do
+        ngx.say("[GET ] key:", k, " v:", v)
+    end
+
+    --POST arg
+    ngx.req.read_body() -- 解析 body 参数之前一定要先读取 body
+    local postArgs = ngx.req.get_post_args()
+    for k, v in pairs(getArgs) do
+        ngx.say("[POST ] key:", k, " v:", v)
+    end
+
+    --POST body
 end
 
---POST arg
-ngx.req.read_body() -- 解析 body 参数之前一定要先读取 body
-local postArgs = ngx.req.get_post_args()
-for k, v in pairs(getArgs) do
-    ngx.say("[POST ] key:", k, " v:", v)
-end
-
---POST body
-
-ngx.say("??????????!!!???")
+return _M
