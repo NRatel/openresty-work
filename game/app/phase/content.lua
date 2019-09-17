e8e2efce032887c82a9307e9af84df9f8478cdc6
聂红强 <36918549+NRatel@users.noreply.github.com>
2019-09-17 13:47:26 +0800

    Update content.lua

--- game/app/phase/content.lua
+++ game/app/phase/content.lua
@@ -12,45 +12,7 @@
 
 --------------------------------------------------------------------------
 
--- require("app.test.request_arg")
-
--- require("app.test.pb")
-
-local tmysql = require("app.test.mysql")
-tmysql:test();
-
--- require("app.test.redis")
-
--- local mysql = require "resty.mysql"
--- local db, err = mysql:new()
--- if not db then
---     ngx.say("failed to instantiate mysql: ", err)
---     return
--- end
-
--- db:set_timeout(1000) -- 1 sec
-
--- -- or connect to a unix domain socket file listened
--- -- by a mysql server:
--- --     local ok, err, errcode, sqlstate =
--- --           db:connect{
--- --              path = "/path/to/mysql.sock",
--- --              database = "ngx_test",
--- --              user = "ngx_test",
--- --              password = "ngx_test" }
--- local ok, err, errcode, sqlstate = db:connect {
---     host = "127.0.0.1",
---     port = 3306,
---     database = "test",
---     user = "test",
---     password = "test",
---     charset = "utf8",
---     max_packet_size = 1024 * 1024,
--- }
-
--- if not ok then
---     ngx.say("failed to connect: ", err, ": ", errcode, " ", sqlstate)
---     return
--- end
-
--- ngx.say("connected to mysql.")
+-- require("app.test.request_arg"):test()
+-- require("app.test.pb"):test()
+require("app.test.mysql"):test()
+-- require("app.test.redis"):test()
